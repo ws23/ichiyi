e15dc275b4cd56085f7d3366afa3c2fbeeee5cca
ws育慈 <linyutsu23@gmail.com>
2014-08-14 17:30:44 +0800

    do the "news" setting

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <hr />
-<p align="center" style="color: #AAAAAA; font-size: 12pt; "><a href="article/?q=node/11">公司介紹</a> | <a href="article/?q=node/12">聯絡我們</a> | <a href="article/?q=node/13">招聘英才</a> | <a href="article/?q=user/login">網站管理</a><br />
+<p align="center" style="color: #AAAAAA; font-size: 12pt; "><a href="article/?q=news">最新消息</a> | <a href="article/?q=info">公司介紹</a> | <a href="article/?q=communicate">聯絡我們</a> | <a href="article/?q=job">招聘英才</a> | <a href="article/?q=user/login">網站管理</a><br />
 愛奇藝 ◎ <a href="index.php">2014 iChiYi.com.tw</a></p>
 </div>
 </body>

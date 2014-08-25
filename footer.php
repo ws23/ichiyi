10f7b4083ab21bb758ede275a4d406a62e9ba7ad
ws23 <ws23@csie-sa.ndhu-csie.twbbs.org>
2014-08-25 13:32:18 +0800

    update logo, index redirect

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <hr />
-<p align="center" style="color: #AAAAAA; font-size: 12pt; "><a href="article/?q=info">公司介紹</a> | <a href="article/?q=news">最新消息</a> | <a href="article/?q=customers">客戶列表</a> | <a href="article/?q=communicate">聯絡方式</a> | <a href="article/?q=user/login">網站管理</a><br />
+<p align="center" style="color: #AAAAAA; font-size: 12pt; "><a href="article/?q=info">公司介紹</a> | <a href="article/?q=news">最新消息</a> | <a href="article/?q=customers">客戶列表</a> | <a href="article/?q=communicate">聯絡方式</a><br />
 愛奇藝 ◎ <a href="index.php">2014 iChiYi.com.tw</a></p>
 </div>
 </body>
